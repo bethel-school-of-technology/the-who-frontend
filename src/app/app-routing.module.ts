da669f16957fd06e602987e28b85ba75c94e0c4c
Joye Davis <joye@pureheartministries.net>
2019-06-09 18:38:29 -0700

    added events page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,8 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  { path: 'home', loadChildren: './home/home.module#HomePageModule' },  { path: 'events', loadChildren: './events/events.module#EventsPageModule' },
+
 ];
 
 @NgModule({
