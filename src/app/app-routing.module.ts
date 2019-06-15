b2843ef2bc03e2077596e3578ae749250332cc7c
Joye Davis <joye@pureheartministries.net>
2019-06-14 21:21:14 -0700

    create post html

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },  { path: 'events', loadChildren: './events/events.module#EventsPageModule' },
+  { path: 'create-post', loadChildren: './create-post/create-post.module#CreatePostPageModule' },
 
 ];
 
---
+++ src/app/create-post/create-post.page.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Post } from '../post.model';
+
+@Component({
+  selector: 'app-create-post',
+  templateUrl: './create-post.page.html',
+  styleUrls: ['./create-post.page.scss'],
+})
+export class CreatePostPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
