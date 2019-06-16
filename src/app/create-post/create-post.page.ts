1fa6cc2d8e6c5bab2f1275204d64cbcb40786bde
Joye Davis <joye@pureheartministries.net>
2019-06-16 13:32:16 -0700

    framework of html

--- src/app/create-post/create-post.page.ts
+++ src/app/create-post/create-post.page.ts
@@ -1,6 +1,9 @@
+
 import { Component, OnInit } from '@angular/core';
 
 import { Post } from '../post.model';
+
+
 
 @Component({
   selector: 'app-create-post',
@@ -8,6 +11,13 @@ import { Post } from '../post.model';
   styleUrls: ['./create-post.page.scss'],
 })
 export class CreatePostPage implements OnInit {
+  post: any = {
+    post_id: Number,
+    user_id: Number,
+    title: '',
+    body: '',
+    create_date: Date
+  };
 
   constructor() { }
 
